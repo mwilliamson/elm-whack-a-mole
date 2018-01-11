73bd094a7bf67fa7f421b4e0203c069cd5892884
Michael Williamson <mike@zwobble.org>
2018-01-11 23:17:13 +0000

    Render unused squares

--- src/Main.elm
+++ src/Main.elm
@@ -19,16 +19,33 @@ main =
 
 
 type alias Model =
-  { width : Int
+  { activeRadius : Int
   }
   
-width : Model -> Int
-width model = model.width
+activeWidth : Model -> Int
+activeWidth model = model.activeRadius * 2 - 1
 
+maxWidth : Model -> Int
+maxWidth model = 5
+
+gridIndices : Model -> List Int
+gridIndices model = List.range 0 ((maxWidth model) - 1)
+
+isActive : Model -> Int -> Int -> Bool
+isActive model rowIndex columnIndex =
+  isActiveIndex model rowIndex && isActiveIndex model columnIndex
+
+isActiveIndex : Model -> Int -> Bool
+isActiveIndex model index =
+  let
+    left = ((maxWidth model) - (activeWidth model)) // 2
+    right = left + (activeWidth model) - 1
+  in
+    index >= left && index <= right
 
 init : (Model, Cmd Msg)
 init =
-  (Model 3, Cmd.none)
+  ({ activeRadius = 2 }, Cmd.none)
 
 
 
@@ -57,11 +74,20 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  let
-    row = viewRow (List.repeat (width model) viewEmptySquare)
-    grid = div [] (List.repeat (width model) row)
-  in
-  grid
+  div [] (List.map (viewModelRow model) (gridIndices model))
+
+
+viewModelRow : Model -> Int -> Html Msg
+viewModelRow model rowIndex =
+  viewRow (List.map (viewModelSquare model rowIndex) (gridIndices model))
+
+
+viewModelSquare : Model -> Int -> Int -> Html Msg
+viewModelSquare model rowIndex columnIndex =
+  if isActive model rowIndex columnIndex then
+    viewEmptySquare
+  else
+    viewUnusedSquare
 
 
 viewRow : List (Html Msg) -> Html Msg
@@ -72,17 +98,22 @@ viewEmptySquare : Html Msg
 viewEmptySquare = viewSquare "#eee"
 
 
+viewUnusedSquare : Html Msg
+viewUnusedSquare = viewSquare "#fff"
+
+
 viewSquare : String -> Html Msg
 viewSquare color =
-  let width = "40px"
+  let
+    width = "40px"
   in
-  div
-    [ style
-      [ ("float", "left")
-      , ("backgroundColor", color)
-      , ("border", "1px solid #fff")
-      , ("width", width)
-      , ("height", width)
+    div
+      [ style
+        [ ("float", "left")
+        , ("backgroundColor", color)
+        , ("border", "1px solid #fff")
+        , ("width", width)
+        , ("height", width)
+        ]
       ]
-    ]
-    []
+      []
